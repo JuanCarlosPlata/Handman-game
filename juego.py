bd10206f0488c6a9799a00238bfe2234c32180e5
JuanCarlosPlata <platapradajuancarlos@gmail.com>
2023-03-10 18:25:30 +0000

    funciones esenciales listas

--- juego.py
+++ juego.py
@@ -3,7 +3,23 @@ import sys
 from os import system, name
 import random
 
-def clear():
+
+
+def hangman(rope="",head="",chest="",right_arm=" ",left_arm="",cintura="",right_leg="",left_leg=""):
+    stickman=f"""
+   ________
+    |/   {rope}     
+    |   {head}    
+    |   {right_arm}{chest}{left_arm}           
+    |    {cintura}        
+    |   {right_leg} {left_leg}        
+    |               
+    |___           
+    """
+    print(stickman)
+
+# Funcion para limpiar la pantalla
+def clear_screen():
     # for windows
     if name == 'nt':
         _ = system('cls')
@@ -21,30 +37,49 @@ def delay_print(s):
 # Funcion para validadr que solo se ingresen letras en el input del usuario
 def validate_user_input_text(text):
     while True:
-        try:
-            delay_print(text)
-            valor = int(input("\n=> "))
-        except ValueError:
-            delay_print(f"Invalid option\nPlease only enter letters (A-Z)\n")
-            sleep(1.5)
-            continue
-        return valor
+        # text indica el mensaje que se mostrar junto con el input
+        delay_print(text)
+        valor = str(input("\n=> "))
+        # Si lo ingresado es solo 1 letra y no es nada distinto a letras el programa continua de los contrario lanza un error y vuelve a preguntar
+        if len(valor) == 1 and valor.isalpha():
+            return valor
+        delay_print(f"Invalid option\nPlease only enter 1 letter (A-Z)\n")
+        sleep(1)
+        continue
 
-# Funcion que escoge una palbra de la lista
-def words():
-    words = ["laptop", "umbrella", "radiator", "kitchen", "smartphone"]
-    word = random.choice(words)
-    return word
+# Funcion que escoge una palabra de formar aleatoria del archivo palabras.txt
+def choice_word():
+    with open('palabras.txt') as f:
+        words = f.read().split(",")
+        my_pick = random.choice(words)
+    return my_pick
 
 # Funcion que mostrara en pantalla el juego
-def visual_game():
-    None
+def visual_game(lives):
+    print(hangman[lives])
 
-word =  words()
-lifes = 6
+# Funcionn para comprobar que la letrea ingresada se encuentre en la  palabra a adivinar
+def check_user_input_letter(letra):
+    for i, caracter in enumerate(word):
+        if caracter == letra:
+            secret_word[i] = letra
+    return secret_word
+
+# variables de la palabra escogida y la palbra secreta
+word = choice_word()
+list_word = list(word)
+secret_word = ["_"]*len(word)
+
 # nucleo del juego
 def run():
+    lifes = 7
+    used_words;used_words = "".capitalize
+    ronda;ronda=0
     while lifes < 0:
+        hangman()
+        validate_user_input_text("Please enter one letter (A-Z)")
+        continue
+        
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from time import sleep
 import sys
 from os import system, name
 import random
-import pandas
+import pandas as pd
 
-def words():
+"""def words():
     words = ["laptop", "umbrella", "radiator", "kitchen", "smartphone"]
     word = random.choice(words)
     return word
@@ -12,10 +12,87 @@ def words():
 
 word =  words()
 print (f"{word}\n{len(word)}")
-
+"""
 
 # reading the CSV file
-csvFile = pandas.read_csv('Giants.csv')
+"""csvFile = pd.read_csv('palabras.csv')"""
+"""word = random.choice(csvFile)"""
 
 # displaying the contents of the CSV file
-print(csvFile)
+"""print(csvFile)"""
+"""print(word)"""
+
+
+"""with open('palabras.txt') as f:
+    words = f.read().split(",")
+    my_pick = random.choice(words)
+    print (my_pick)"""
+
+
+
+
+  
+"""def word():
+    with open('palabras.txt') as f:
+        words = f.read().split(",")
+        my_pick = random.choice(words)
+    return my_pick
+
+print (word())"""
+
+def choice_word():
+    with open('palabras.txt') as f:
+        words = f.read().split(",")
+        my_pick = random.choice(words)
+    return my_pick
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        sleep(0.02)  # Tiempo de espera estre cada letra
+
+def validate_user_input_text(text):
+    while True:
+        # text indica el mensaje que se mostrar junto con el input
+        delay_print(text)
+        valor = str(input("\n=> "))
+        # Si lo ingresado es solo 1 letra y no es nada distinto a letras el programa continua de los contrario lanza un error y vuelve a preguntar
+        if len(valor) == 1 and valor.isalpha():
+            return valor
+        delay_print(f"Invalid option\nPlease only enter 1 letter (A-Z)\n")
+        sleep(1)
+        continue
+
+"""print(validate_user_input_text("Ingrese una letra"))"""
+
+
+
+word = choice_word()
+list_word = list(word)
+secret_word = ["_"]*len(word)
+
+def check_user_input_letter(letra):
+    for i, caracter in enumerate(word):
+        if caracter == letra:
+            secret_word[i] = letra
+    return secret_word
+
+
+
+
+def hangman(rope="",head="",chest="",right_arm=" ",left_arm="",cintura="",right_leg="",left_leg=""):
+    stickman=f"""
+   ________
+    |/   {rope}     
+    |   {head}    
+    |   {right_arm}{chest}{left_arm}           
+    |    {cintura}        
+    |   {right_leg} {left_leg}        
+    |               
+    |___           """
+    print(stickman)
+
+hangman("|","🐷","|","/","\\","|","/","\\")
+
+print(" "*10,"".join(check_user_input_letter("o")))
